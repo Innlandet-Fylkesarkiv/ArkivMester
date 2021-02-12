35dee4da54f61d0e8ddff954650ccc428bdd9ecc
tobiae <tobiae@stud.ntnu.no>
2021-02-12 11:11:00 +0100

    no changes

---
+++ src/arkivmester/ExceptionHandler.java
@@ -0,0 +1,23 @@
+package arkivmester;
+
+import org.w3c.dom.Document;
+
+import java.io.FileNotFoundException;
+
+public class ExceptionHandler extends Exception {
+
+    public static tryOpeningFile() {
+        Document doc;
+        try {
+            doc = parseFromXMLFile(file);
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());                // NOSONAR
+            missingFile("file not found") throw Exception; // NOSONAR
+        }
+    }
+
+    public static void missingFile(String message) throws FileNotFoundException {  //constructor
+        throw new FileNotFoundException("file is empty");
+    }
+
+}
