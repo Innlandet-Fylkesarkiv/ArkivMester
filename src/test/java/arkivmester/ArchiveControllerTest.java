1ee5ba7b8317878c32e0e86bf97197104b291fa4
Magnus Sustad <magnsus@stud.ntnu.no>
2021-02-25 12:54:39 +0100

    Added test folder

---
+++ src/test/java/arkivmester/ArchiveControllerTest.java
@@ -0,0 +1,12 @@
+package arkivmester;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArchiveControllerTest {
+
+    @Test
+    void start() { // #NOSONAR
+    }
+}
