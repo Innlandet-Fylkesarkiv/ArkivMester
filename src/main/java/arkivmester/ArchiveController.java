c06b96392e4f973646d932657f92b0ad6e30702e
Esben Lomholt Bjarnason <esbenlb@stud.ntnu.no>
2021-04-11 14:41:13 +0200

    remove test function

--- src/main/java/arkivmester/ArchiveController.java
+++ src/main/java/arkivmester/ArchiveController.java
@@ -44,7 +44,6 @@ public class ArchiveController implements ViewObserver {
         thirdPartiesModel = new ThirdPartiesModel();
         settingsModel = new SettingsModel();
         reportModel = new ReportModel();
-        ArkadeModel arkadeModel = new ArkadeModel(); // Todo remove
     }
 
     /**
